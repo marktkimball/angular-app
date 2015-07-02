3318936268f1e80d1094b4bc3d3d7c42d2c1a160
Mark Kimball <parkskier@gmail.com>
2015-07-02 16:49:58 -0400

    tempature posting to html

--- controller.js
+++ controller.js
@@ -4,11 +4,8 @@
     .module('weatherApp')
     .controller('MainController', function($scope, WeatherService){
         $scope.getWeather = function(newAddress, time){
-          console.log("request sent, " + newAddress + " time " + time);
-          var time = time;
-
+          var time = Math.floor(time/1000);
           var addressString = newAddress;
-
           var geocoder = new google.maps.Geocoder();
 
           geocoder.geocode( { 'address': addressString}, function(results, status) {
@@ -17,7 +14,7 @@
               var latitude = results[0].geometry.location.lat();
               var longitude = results[0].geometry.location.lng();
               WeatherService.read(latitude, longitude, time).success(function(data){
-               console.log(data);
+                $scope.data = data;
               });
             }
           });
