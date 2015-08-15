a743fd6ff878c20998c7fe01f423808f6cf880e6
Mark Kimball <parkskier@gmail.com>
2015-08-15 15:48:42 -0400

    scss added, more responsive

--- controller.js
+++ controller.js
@@ -92,33 +92,9 @@
 
           $scope.days = days;
 
-          var hoursOfDay = {
-            0: "12 AM",
-            1: "1 AM",
-            2: "2 AM",
-            3: "3 AM",
-            4: "4 AM",
-            5: "5 AM",
-            6: "6 AM",
-            7: "7 AM",
-            8: "8 AM",
-            9: "9 AM",
-            10: "10 AM",
-            11: "11 AM",
-            12: "12 PM",
-            13: "1 PM",
-            14: "2 PM",
-            15: "3 PM",
-            16: "4 PM",
-            17: "5 PM",
-            18: "6 PM",
-            19: "7 PM",
-            20: "8 PM",
-            21: "9 PM",
-            22: "10 PM",
-            23: "11 PM",
-            24: "12 PM"
-          }
+          var hoursOfDay = [
+            "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 PM"
+          ];
 
           var hours = [];
           for(var j = 0; j < 7; j++){
